a63af91255c4946199e0cbc2d58f647bc4ff926c
Jeka Der <jekader@gmail.com>
2014-11-27 21:25:37 +0100

    moving on

--- arduino/vaz2103mon.ino
+++ arduino/vaz2103mon.ino
@@ -4,41 +4,43 @@
  
   This example code is in the public domain.
  */
+int ledPin = 13;
 
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
   pinMode(13, OUTPUT);     
+  //init the PWM pins
+  pinMode(3, OUTPUT); 
+  pinMode(5, OUTPUT); 
+  pinMode(6, OUTPUT); 
+  pinMode(9, OUTPUT); 
+  pinMode(10, OUTPUT); 
   pinMode(11, OUTPUT); 
+  //init serial
+  Serial.begin(9600);
 }
 
 void loop() {
 
+// this part is from http://www.suffix.be/blog/send-data-to-arduino
+// some info on PWM here: http://www.uchobby.com/index.php/2008/02/12/arduino-analog-gauge/
+// need to add resistors
+ if (Serial.available() > 0) {
+//    char value = Serial.read();
+    int value = Serial.parseInt();
+    if (value > 1) {
+      if (value < 256) {
+        analogWrite(11, value); 
+      }
+    }
+/*    if (value == '1') {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    } */
+    Serial.println(value);
+  }
 
-for (int i=1; i<15; i+=1)
-{  
-  for(int j=1; j<x;j++)
-   {   
-     digitalWrite(13, HIGH);   // set the LED on
-     delay(10);              // wait for a second
-     digitalWrite(13, LOW);    // set the LED off
-     delay(i);  // wait for a second
-
-   }
-   int qq=i*10;
-   analogWrite(11, qq);   
-}
-for (int i=15; i>1; i-=1)
-{ 
-  for(int j=1; j<x;j++)
-   { 
-    digitalWrite(13, HIGH);   // set the LED on
-    delay(10);              // wait for a second
-    digitalWrite(13, LOW);    // set the LED off
-    delay(i);  // wait for a second
-   }
-   int qq=i*10;
-   analogWrite(11, qq);   
-}
 }
 
