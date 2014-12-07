18c3ed29567e040e937cea00faf28db69d684f0c
Jeka Der <jekader@gmail.com>
2014-12-07 23:08:02 +0100

    stepper experiments

--- arduino/vaz2103mon/vaz2103mon.ino
+++ arduino/vaz2103mon/vaz2103mon.ino
@@ -4,6 +4,15 @@
   This code is in the public domain.
  */
 
+// driving cd-rom stepper, based on
+// http://electronics-diy.com/driving-cdrom-stepper-motor-with-arduino.php
+
+int coila1 = 2; 
+int coila2 = 4; 
+int coilb1 = 7; 
+int coilb2 = 8;
+int mdelay = 50;
+
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
@@ -15,12 +24,51 @@ void setup() {
   pinMode(9, OUTPUT); 
   pinMode(10, OUTPUT); 
   pinMode(11, OUTPUT); 
+  pinMode(coila1, OUTPUT); 
+  pinMode(coila2, OUTPUT); 
+  pinMode(coilb1, OUTPUT); 
+  pinMode(coilb2, OUTPUT); 
+
   //init serial
   Serial.begin(9600);
 }
 
 void loop() {
 
+  
+// stepper experiments. Commented out for now
+//for (int i=0; i <= 255; i++){
+
+//digitalWrite(coila1, HIGH); 
+//digitalWrite(coila2, LOW); 
+//digitalWrite(coilb1, LOW); 
+//digitalWrite(coilb2, LOW); 
+//delay(mdelay); 
+
+//digitalWrite(coila1, LOW); 
+//digitalWrite(coila2, LOW); 
+//digitalWrite(coilb1, HIGH); 
+//digitalWrite(coilb2, LOW); 
+//delay(mdelay); 
+
+//digitalWrite(coila1, LOW); 
+//digitalWrite(coila2, HIGH); 
+//digitalWrite(coilb1, LOW); 
+//digitalWrite(coilb2, LOW); 
+//delay(mdelay); 
+
+//digitalWrite(coila1, LOW); 
+//digitalWrite(coila2, LOW); 
+//digitalWrite(coilb1, LOW); 
+//digitalWrite(coilb2, HIGH); 
+//delay(mdelay); 
+//}
+
+//digitalWrite(coila1, LOW); 
+//digitalWrite(coila2, LOW); 
+//digitalWrite(coilb1, LOW); 
+//digitalWrite(coilb2, LOW); 
+  
 // some info on PWM here: http://www.uchobby.com/index.php/2008/02/12/arduino-analog-gauge/
 // need to add resistors
  if (Serial.available() > 0) {
@@ -30,6 +78,7 @@ void loop() {
     int cpu = Serial.parseInt();
     int ram = Serial.parseInt();    
     int net = Serial.parseInt(); 
+
     if (Serial.read() == '\n') {
       
       if (cpu >= 0) {
