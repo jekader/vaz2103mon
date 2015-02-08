72e2792a037187ebc105301dfe3d5d24336166d5
Jeka Der <jekader@gmail.com>
2015-02-08 20:45:39 +0100

    adding steper controls for speedometer

--- arduino/vaz2103mon/vaz2103mon.ino
+++ arduino/vaz2103mon/vaz2103mon.ino
@@ -7,23 +7,32 @@
 // driving cd-rom stepper, based on
 // http://www.edaboard.com/thread217270.html
 
+// define stepper pins (they don't need to be PWM-capable)
+// I'm using a 28YBT-48 stepper
+// some info is available here: http://www.utopiamechanicus.com/article/arduino-stepper-motor-setup-troubleshooting/
 int coila1 = 2; 
 int coila2 = 4; 
-int coilb1 = 7; 
-int coilb2 = 8;
+int coilb1 = 3; 
+int coilb2 = 5;
+// delay between steps
 int mdelay = 10;
+
+// define PWM pins for remaining gauges
+int rampin = 9;
+int netpin = 10;
+int temppin = 11;
+int iopin = 6;
 
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
   pinMode(13, OUTPUT);     
-  //init the PWM pins
-  pinMode(3, OUTPUT); 
-  pinMode(5, OUTPUT); 
+  // init PWM pins
   pinMode(6, OUTPUT); 
   pinMode(9, OUTPUT); 
   pinMode(10, OUTPUT); 
   pinMode(11, OUTPUT); 
+  // init stepper pins
   pinMode(coila1, OUTPUT); 
   pinMode(coila2, OUTPUT); 
   pinMode(coilb1, OUTPUT); 
@@ -35,13 +44,7 @@ void setup() {
 
 void loop() {
 
-  
-// stepper experiments.
-// int stp;
-// stp = stepperMove(1,20);
-// stp = stepperMove(-1,20);
-
-// some info on PWM here: http://www.uchobby.com/index.php/2008/02/12/arduino-analog-gauge/
+  // some info on PWM here: http://www.uchobby.com/index.php/2008/02/12/arduino-analog-gauge/
 // need to add resistors
  if (Serial.available() > 0) {
 // parsing values http://arduino.cc/en/Tutorial/ReadASCIIString
@@ -50,22 +53,50 @@ void loop() {
     int cpu = Serial.parseInt();
     int ram = Serial.parseInt();    
     int net = Serial.parseInt(); 
+    int temp = Serial.parseInt(); 
+    int io = Serial.parseInt(); 
 
     if (Serial.read() == '\n') {
       
-      if (cpu >= 0) {
-        if (cpu < 256) {
-          analogWrite(11, cpu); 
+      if (cpu >= -2000) {
+        if (cpu < 2000) {
+          int sdirection = -1;
+          int modcpu = cpu;
+          if (cpu < 0) {
+              sdirection = 1;
+              modcpu = -1 * cpu;
+          }
+          // move only if there's more than 0 steps
+          if (modcpu > 0) {
+            stepperMove(sdirection,modcpu);
+          }
+          // otherwise disable all stepper pins
+          else {
+            digitalWrite(coila1, LOW); 
+            digitalWrite(coila2, LOW); 
+            digitalWrite(coilb1, LOW); 
+            digitalWrite(coilb2, LOW);
+          } 
         }
       }
       if (ram >= 0) {
         if (ram < 256) {
-          analogWrite(10, ram); 
+          analogWrite(rampin, ram); 
         }
       }
       if (net >= 0) {
         if (net < 256) {
-          analogWrite(9, net); 
+          analogWrite(netpin, net); 
+        }
+      }
+      if (temp >= 0) {
+        if (temp < 256) {
+          analogWrite(temppin, net); 
+        }
+      }
+      if (io >= 0) {
+        if (io < 256) {
+          analogWrite(iopin, net); 
         }
       }
       String gauge_name = " CPU:";
@@ -79,9 +110,15 @@ void loop() {
       gauge_name = " NET:";
       gauge_value = String(net);
       printed_line = gauge_name + gauge_value;
+      Serial.print(printed_line);
+      gauge_name = " TEMP:";
+      gauge_value = String(temp);
+      printed_line = gauge_name + gauge_value;
+      Serial.print(printed_line);
+      gauge_name = " IO:";
+      gauge_value = String(io);
+      printed_line = gauge_name + gauge_value;
       Serial.println(printed_line);
-
-
     }
  }
 }
--- computer/vaz2103mon.sh
+++ computer/vaz2103mon.sh
@@ -2,15 +2,47 @@
 # Status script for wmfs
 # based on https://bbs.archlinux.org/viewtopic.php?pid=872150#p872150
 
-ttyport="/dev/ttyUSB1"
+ttyport="/dev/ttyUSB0"
+
+# the dials on the instrument panel are as follows:
+# бензин вода  масло тахометр   спидометр
+#
+# gas    water oil   tachometer speedometer
+#
+# sizes
+#
+# small  small small large      large
+#
+# driven by
+#
+# PWM    PWM   PWM   PWM?       stepper
+#
+# data
+#
+# I/O    temp  net   RAM        CPU
+#
+# Arduino pins
+# ?      ?     9     10          11 
+# 6      11    10    9         2,3,4,5
+
 
 # define ranges for PWM
-CPUMIN=100
-CPUMAX=0
-RAMMIN=85
-RAMMAX=0
-NETMIN=0
-NETMAX=200
+# CPU -> large stepper controlled dial
+CPUMIN=0
+CPUMAX=500
+PREVCPUz=0
+# RAM -> large PWM controlled dial
+RAMMIN=0
+RAMMAX=200
+# net -> small PWM controlled dial
+NETMIN=85
+NETMAX=0
+# temp -> small PWM controlled dial
+TEMPMIN=100
+TEMPMAX=0
+# I/O -> small PWM controlled dial
+IOMIN=100
+IOMAX=0
 #define max bandwidth and interface to monitor (I use a DSL router so link speed doesn't give much)
 net_max_downlink_mbps=40
 uplink_if=br0
@@ -20,7 +52,9 @@ uplink_if=br0
 CPUf=$( echo "scale=1; ($CPUMAX-$CPUMIN)/100" | bc)
 RAMf=$( echo "scale=1; ($RAMMAX-$RAMMIN)/100" | bc)
 NETf=$( echo "scale=1; ($NETMAX-$NETMIN)/100" | bc)
-echo "constants:  $CPUf $RAMf "
+TEMPf=$( echo "scale=1; ($TEMPMAX-$TEMPMIN)/100" | bc)
+IOf=$( echo "scale=1; ($IOMAX-$IOMIN)/100" | bc)
+echo "constants:  CPUf: $CPUf RAMf: $RAMf NETf: $NETf TEMPf: $TEMPf IOf: $IOf"
 echo "initializing $ttyport"
 #initialize port
 stty -F $ttyport 9600 cs8 -cstopb
@@ -35,13 +69,13 @@ do
 
 echo -n "$Count."
 # all zeroes
-ARDCPU="$CPUMIN,$RAMMIN,$NETMIN"
+ARDCPU="-100,$RAMMIN,$NETMIN,$TEMPMIN,$IOMIN"
 echo  $ARDCPU >&3
-sleep 2
+sleep 5
 
 echo -n "."
 # all 100%
-ARDCPU="$CPUMAX,$RAMMAX,$NETMAX"
+ARDCPU="10,$RAMMAX,$NETMAX,$TEMPMAX,$IOMAX"
 echo  $ARDCPU >&3
 sleep 2
 
@@ -81,14 +115,18 @@ fi
 #echo "net debug: $newbytes - $oldbytes = $diffbytes bytes > $diffmegabits mbps > $NET %"
 
 # write to Serial
-echo "RAW:" $CPU $RAM $NET
-CPUzi=$(echo "$CPUMIN+$CPU*$CPUf"|bc)
+echo "RAW:" $PREVCPUz $CPU $RAM $NET
+CPUzi=$(echo "$CPU*$CPUf - $PREVCPUz"|bc)
 CPUz=$(  echo $CPUzi | awk '{print int($1)}' )
+PREVCPUz=$(echo "$CPU*$CPUf"|bc)
+
 RAMzi=$(echo "$RAMMIN+$RAM*$RAMf"|bc)
 RAMz=$(  echo $RAMzi | awk '{print int($1)}' )
+
 NETzi=$(echo "$NETMIN+$NET*$NETf"|bc)
 NETz=$(  echo $NETzi | awk '{print int($1)}' )
-ARDCPU="$CPUz,$RAMz,$NETz"
+
+ARDCPU="$CPUz,$RAMz,$NETz,0,0"
 echo  $ARDCPU >&3
 echo "SND:" $ARDCPU
 echo -n "RCV:"
